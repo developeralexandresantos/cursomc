e452949c45714a70b0b8b909699d51e272ce41cc
Alexandre Aparecido dos Santos <skeeter.182@uninove.edu.br>
2023-09-27 15:53:36 -0300

    Alterado comentario, AB#6

--- src/main/java/br/com/developeralexandresantos/cursomc/resources/PedidoResource.java
+++ src/main/java/br/com/developeralexandresantos/cursomc/resources/PedidoResource.java
@@ -15,6 +15,7 @@ import br.com.developeralexandresantos.cursomc.services.PedidoService;
 public class PedidoResource {
 	/**
 	 * author: Alexandre A Santos
+	 * Fixes the card 06 - test for the Azure Boards
 	 */
 	@Autowired
 	private PedidoService service;
