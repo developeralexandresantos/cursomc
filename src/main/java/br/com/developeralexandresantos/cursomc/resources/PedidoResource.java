2592f2c07905b207648dbab0f486407a05aa05cd
Alexandre Aparecido dos Santos <skeeter.182@uninove.edu.br>
2023-09-27 17:45:48 -0300

    trying to move again, review AB#11

--- src/main/java/br/com/developeralexandresantos/cursomc/resources/PedidoResource.java
+++ src/main/java/br/com/developeralexandresantos/cursomc/resources/PedidoResource.java
@@ -15,7 +15,7 @@ import br.com.developeralexandresantos.cursomc.services.PedidoService;
 public class PedidoResource {
 	/**
 	 * author: Alexandre A Santos
-	 * Test to move the task to review
+	 * New Test to move the task to review
 	 */
 	@Autowired
 	private PedidoService service;
